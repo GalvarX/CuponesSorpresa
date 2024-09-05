b385a67e08266f29d7b615a0556ade6697a35adb
GalvarX <alvarezgonzalojulian@gmail.com>
2024-09-04 22:10:51 -0300

    script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function showRiddleModal(riddle, correctAnswer, imageUrl) {
     document.getElementById('riddleInput').dataset.imageUrl = imageUrl;
     document.getElementById('riddleModal').style.display = 'flex';
 
-      // Añadir el manejador de eventos para la tecla Enter
-      document.getElementById('riddleInput').addEventListener('keypress', function(event) {
+    // Añadir el manejador de eventos para la tecla Enter
+    document.getElementById('riddleInput').addEventListener('keypress', function(event) {
         if (event.key === 'Enter') {
             event.preventDefault(); // Evitar el comportamiento por defecto del Enter
             checkAnswer(); // Llamar a la función para verificar la respuesta
@@ -24,15 +24,22 @@ function checkAnswer() {
     const enteredAnswer = riddleInput.value.toLowerCase(); // Convertir la respuesta ingresada a minúsculas
     const correctAnswer = riddleInput.dataset.correctAnswer;
     const imageUrl = riddleInput.dataset.imageUrl;
-    const unlockedImageContainer = document.getElementById('unlockedImageContainer');
 
     if (enteredAnswer === correctAnswer) {
-        const img = document.createElement('img');
-        img.src = imageUrl;
-        img.alt = 'Imagen Desbloqueada';
-        unlockedImageContainer.innerHTML = ''; // Limpiar el contenedor de imágenes previas
-        unlockedImageContainer.appendChild(img);
+        // Encontrar la imagen en la galería que corresponde a la respuesta correcta
+        const imgElement = Array.from(document.querySelectorAll('.gallery img')).find(img => img.dataset.answer.toLowerCase() === correctAnswer.toLowerCase());
+
+        if (imgElement) {
+            imgElement.src = imageUrl; // Reemplazar el src de la imagen
+            imgElement.alt = 'Imagen Desbloqueada'; // Actualizar el alt de la imagen
+        } else {
+            console.error('No se encontró la imagen correspondiente para reemplazar.');
+        }
+
+        closeModal(); // Cerrar el modal al responder correctamente
     } else {
+        const unlockedImageContainer = document.getElementById('unlockedImageContainer');
         unlockedImageContainer.innerHTML = '<p>Respuesta incorrecta. Inténtalo de nuevo.</p>';
     }
 }
+
